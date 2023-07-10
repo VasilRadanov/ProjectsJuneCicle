cfbbccd93ccabda291b8b9e955e3b5b6e686561b
VasilRadanov <woreldruler@gmail.com>
2023-07-10 19:28:10 +0300

    .

--- src/QuizGame.java
+++ src/QuizGame.java
@@ -89,8 +89,24 @@ public class QuizGame {
         for (String[] question : questions) {
             displayQuestion(question);
 
-            int answerIndex = scanner.nextInt();
-            scanner.nextLine();
+            int answerIndex = 0;
+            boolean validInput = false;
+
+            while (!validInput) {
+                System.out.print("Enter your answer (1-4): ");
+                String input = scanner.nextLine();
+
+                try {
+                    answerIndex = Integer.parseInt(input);
+                    if (answerIndex >= 1 && answerIndex <= 4) {
+                        validInput = true;
+                    } else {
+                        System.out.println("Invalid answer index. Please enter a number between 1 to 4.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a number between 1 to 4.");
+                }
+            }
 
             if (answerIndex == 1) {
                 System.out.println("Correct!\n");
